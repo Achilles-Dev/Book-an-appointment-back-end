f4c890554806c8751f65ea329007d2f59cbeff07
Achilles-Dev <solohag92@gmail.com>
2023-01-13 19:08:55 +0000

    Update Motorcycle model

--- app/models/motorcycle.rb
+++ app/models/motorcycle.rb
@@ -1,6 +1,7 @@
 class Motorcycle < ApplicationRecord
   has_one_attached :image, dependent: :destroy
   belongs_to :user
+  has_many :reservations, dependent: :destroy
   validates :model, presence: true
   validates :description, presence: true, length: { in: 1..10_000 }
   validates :duration_months, presence: true, numericality: { greater_than: 0 }
